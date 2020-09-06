6f8db86424408507fcfb07ecd375f67e1099bb53
Mason Moreland <mazc@protonmail.ch>
2020-09-06 00:58:16 -0700

    Add 1480

---
+++ leetcode/e_1480.cpp
@@ -0,0 +1,21 @@
+/*
+Problem: 1480 - Running Sum of 1d Array
+Difficulty: Easy
+URL: https://leetcode.com/problems/running-sum-of-1d-array/
+*/
+
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> runningSum(std::vector<int>& nums)
+    {
+        int i = 1;
+        while (i < nums.size())
+        {
+            nums[i] += nums[i-1];
+            i++;
+        }
+        return nums;
+    }
+};
